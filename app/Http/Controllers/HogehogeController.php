c262a4553df41b1d7de542aa50993fb01260cebb
DJMasaru <otsusyarepapa@gmail.com>
2022-12-04 01:03:38 +0900

    基本的なCRUD処理の理解

---
+++ app/Http/Controllers/HogehogeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Hogehoge;
+use Illuminate\Http\Request;
+
+class HogehogeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Hogehoge  $hogehoge
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Hogehoge $hogehoge)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Hogehoge  $hogehoge
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Hogehoge $hogehoge)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Hogehoge  $hogehoge
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Hogehoge $hogehoge)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Hogehoge  $hogehoge
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Hogehoge $hogehoge)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// app.blade.phpはレイアウト。実のデータを持ってくるためにはdashboardをリンク先として指定すること。
+// Route::get('/', function () {
+//     return view('layouts/dashboard');
+// });
+
+// ①リンク先で/が指定されたらarticleという名前がついたページに遷移する
+Route::redirect('/', 'article');
+
+// ②articleという名前がついたページにいくと、ArticleControllerのクラスが呼び出される
+Route::resource('article', ArticleController::class);
